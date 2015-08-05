23e823ae6e445b74b4b0ff329befe82746bdc0a7
dcouwenb <Dimitri.Couwenberg@macquarie.com>
2015-08-05 10:07:49 +1000

    fixed sorting and displaying nr results

--- app/modules/common/orders/controllers/ordersCtrl.js
+++ app/modules/common/orders/controllers/ordersCtrl.js
@@ -1,41 +1,16 @@
-app.controller('OrdersCtrl', function ($scope, ngTableParams, $filter,$location) {
+app.controller('OrdersCtrl', function ($scope, ngTableParams, $filter, $location) {
 
     console.log('ordersctrl time');
 
-    $scope.filtered={};
+    $scope.filtered = {};
 
-    $scope.noOrders =true;
+   /// $scope.noOrders = true;
 
     $scope.tabs = [{
         slug: 'orders',
         title: "Orders",
         contentUrl: '../partials/ordersTab.html'
-    }, {
-        slug: 'room-1',
-        title: "Room 1",
-        content: "Dynamic content 1"
-    }, {
-        slug: 'room-2',
-        title: "Room 2",
-        content: "Dynamic content 2"
     }];
-
-
-    $scope.onTabSelected = function (tab) {
-        var route;
-        if (typeof tab === 'string') {
-            switch (tab) {
-                case 'dashboard':
-                    route = tab;
-                    break;
-                default:
-                    route = 'rooms/' + tab;
-                    break;
-            }
-        }
-        $location.path('/' + route);
-    };
-
 
     $scope.data = [
         {
@@ -433,9 +408,9 @@ app.controller('OrdersCtrl', function ($scope, ngTableParams, $filter,$location)
                 $scope.data;
             $defer.resolve($scope.orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
         }
-            //$scope.accounts = data;
-            ////$defer.resolve($scope.accounts);
-            //$defer.resolve($scope.accounts.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+        //$scope.accounts = data;
+        ////$defer.resolve($scope.accounts);
+        //$defer.resolve($scope.accounts.slice((params.page() - 1) * params.count(), params.page() * params.count()));
 
     })
 
